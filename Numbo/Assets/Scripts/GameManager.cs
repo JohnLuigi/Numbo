55b9433200c6acdd1da003a4099d41b28c8117f5
Shmiddy <eric.rackear@gmail.com>
2017-08-31 20:37:52 -0400

    Added penalty system and layout changes.
    In addition to being able to lose points for submitting the wrong answer
    or going over the goal number, the rules are now shown on the side of
    the screen. Moved the score location to avoid confusion with the goal
    number.

--- Numbo/Assets/Scripts/GameManager.cs
+++ Numbo/Assets/Scripts/GameManager.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public int problemNumber;
+    public int goalNumber;
     public int sum = 0;
     public string pressedKey;
-    public string solvedMessage;
     public int score = 0;
 
 
 
 	// Use this for initialization
 	void Start () {
-        problemNumber = Random.Range(1, 11);
+        goalNumber = Random.Range(1, 11);
 	}
 
     // Update is called once per frame
@@ -22,29 +21,36 @@ public class GameManager : MonoBehaviour {
     {
         if(Input.GetKeyUp("1"))
         {
-            sum += 1;
+            TestSum(1);
         }
 
         if (Input.GetKeyUp("2"))
         {
-            sum += 2;
+            TestSum(2);
         }
 
         if (Input.GetKeyUp("3"))
         {
-            sum += 3;
+            TestSum(3);
         }
 
         // check if the player has added up to the number to solve
         // if right, add one to their score
         if (Input.GetKeyUp("return"))
         {
-            if (sum == problemNumber)
+            if (sum == goalNumber)
             {                
                 score++;
-                solvedMessage = "Correct! Score: " + score;
                 sum = 0;
-                problemNumber = Random.Range(1, 11);
+                goalNumber = Random.Range(1, 11);
+            }
+            // if the wrong answer is submitted, subtract from the score
+            // but don't subtract from the score if no answer has been entered
+            else if (sum != 0)
+            {
+                score--;
+                // TODO
+                // add some sort of visual indication that the wrong answer was submitted
             }
         }
 
@@ -57,11 +63,35 @@ public class GameManager : MonoBehaviour {
 
     void OnGUI()
     {
-        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 100, 30), "Solve: " + problemNumber);
+        // display the number to solve
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 100, 30), "Solve: " + goalNumber);
 
+        // display the working sum
         GUI.Label(new Rect(Screen.width / 2, Screen.height / 2 + (Screen.height / 3), 100, 30), "Answer " + sum);
 
-        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2 - (Screen.height / 3), 100, 30), solvedMessage);
+        // display the score at the top of the screen
+        GUI.Label(new Rect(0, 0, 200, 30), "Score: " + score);
 
+        // display the rules on the side of the screen
+        GUI.Label(new Rect(Screen.width * 0.75f, 0, 200, 400), "Use combinations of 1, 2, and 3 to solve for the number.\nPress enter to submit" +
+        " your answer.\nWrong answers or going over the goal number will cost you a point.");
+
+    }
+
+    // check if the working sum goes over the goal number, if it does, subtract from the score
+    // TODO display a message saying something
+    void TestSum(int value)
+    {
+        if(sum + value > goalNumber)
+        {
+            score--;
+            // TODO add the warning message 
+            // maybe reset the sum to 0 so if the player goes over it they have to start over?
+            sum = 0;
+        }
+        else
+        {
+            sum += value;
+        }
     }
 }
